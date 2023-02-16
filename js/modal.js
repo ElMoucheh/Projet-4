b4f0730da11161a30eda2e73fcf1c66b3762752e
ElMoucheh <williambizien@yahoo.com>
2023-02-16 11:23:00 +0100

    issues form (fname, lname, email, location fixed)

--- js/modal.js
+++ js/modal.js
@@ -15,21 +15,23 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // DOM Input Form
-const iptFirst = document.getElementById("first");
-const iptLast = document.getElementById("last");
-const iptEmail = document.getElementById("email");
-const iptBirthdate = document.getElementById("birthdate");
-const locate = document.getElementsByName("location");
+const chkblocate = document.getElementsByName("location");
+const inpQtt = document.getElementsByName("quantity");
+
+let validForm = {
+  firstname: null,
+  lastname: null,
+  email: null,
+  birthdate: null,
+  tournament: 0,
+  locate: "New York",
+  condition: true,
+  newsletter: false
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 document.querySelector(".close").addEventListener("click", closeModal);
-
-//set location
-locate.forEach((loc) => loc.addEventListener("click", function(loc){
-  selectLocation = loc.target.defaultValue;
-  console.log(selectLocation);
-}));
 
 // launch modal form
 function launchModal() {
@@ -39,4 +41,77 @@ function launchModal() {
 // close modal from
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+// Input validation
+formData.forEach((input) => input.children[2].addEventListener("blur", displayDataError));
+formData.forEach((input) => input.children[2].addEventListener("focus", eraseDataError));
+
+//set location
+chkblocate.forEach((loc) => loc.addEventListener("click", displayDataError));
+
+inpQtt[0].addEventListener("change", displayDataError);
+
+function displayDataError(e){
+
+  switch (e.srcElement.name) {
+    case "first":
+      if(e.srcElement.value.length < 2){
+        formData[0].setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+        formData[0].setAttribute("data-error-visible", "true");
+        validForm["firstname"] = null;
+        console.log(validForm);
+      } else {
+        validForm["firstname"] = e.srcElement.value;
+        console.log(validForm);
+      }
+      break;
+    case "last":
+      if(e.srcElement.value.length < 2){
+        formData[1].setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+        formData[1].setAttribute("data-error-visible", "true");
+        validForm["lastname"] = null;
+        console.log(validForm);
+      } else {
+        validForm["lastname"] = e.srcElement.value;
+        console.log(validForm);
+      }
+      break;
+    case "email":
+      if(!(/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(e.srcElement.value))){
+        formData[2].setAttribute("data-error", "Adresse non conforme.");
+        formData[2].setAttribute("data-error-visible", "true");
+        validForm["email"] = null;
+        console.log(validForm);
+      } else {
+        validForm["email"] = e.srcElement.value;
+        console.log(validForm);
+      }
+      break;
+    case "birthdate":
+      if(e.srcElement.value.length < 2){
+        formData[3].setAttribute("data-error", "Vous devez entrer votre date de naissance.");
+        formData[3].setAttribute("data-error-visible", "true");
+        console.log(validForm);
+      }
+      break;
+
+    case "quantity":
+      validForm["quantity"] = e.srcElement.value;
+      console.log(validForm);
+      break;
+
+    case "location":
+       validForm["locate"] = e.target.defaultValue;
+       console.log(validForm);
+       break;
+
+    default:
+      break;
+  }
+}
+
+function eraseDataError(e){
+  e.target.parentElement.removeAttribute("data-error");
+  e.target.parentElement.removeAttribute("data-error-visible");
 }
